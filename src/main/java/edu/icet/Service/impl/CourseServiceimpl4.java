9526a68da7e32f22c4251ab0e3ce95900ff42761
Chirath Yahampath <141777544+ChirathYahampath@users.noreply.github.com>
2024-01-17 19:16:10 +0530

    Change Attributes

--- src/main/java/edu/icet/Service/impl/CourseServiceimpl4.java
+++ src/main/java/edu/icet/Service/impl/CourseServiceimpl4.java
@@ -17,7 +17,7 @@ public class CourseServiceimpl4 implements CourseServise {
 
         ArrayList<Course> courseList = new ArrayList();
         courseList.add(new Course("Icet java master ",
-                "ICn",
+                "ICk",
                 "70000",
                 "6",institute));
 
